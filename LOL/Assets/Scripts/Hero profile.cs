986c2a277407e4cdf43d03a2f86911cacc380fb8
MFbuider <62496746+MFbuider@users.noreply.github.com>
2020-12-26 17:02:09 +0800

    小地圖

---
+++ LOL/Assets/Scripts/Hero profile.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="英雄資料",menuName = "英雄資料")]
+public class Heroprofile : ScriptableObject
+
+{
+
+}
